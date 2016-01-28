63ef3787ed5d859bca4a583647867c9342eef8e9
= <tsmit291@icloud.com>
2016-01-28 12:18:31 -0700

    it works

--- routes/auth.js
+++ routes/auth.js
@@ -7,8 +7,12 @@ router.get('/linkedin', passport.authenticate('linkedin'));
 
 // route to log out
 router.get('/logout', function(req, res, next){
-  res.session = null
+  req.session = null
   res.redirect('/')
-})
+});
+
+router.get('/linkedin/callback', passport.authenticate('linkedin', { failureRedirect: '/' }), function(req, res, next){
+  res.redirect('/')
+});
 
 module.exports = router;
